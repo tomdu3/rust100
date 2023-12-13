3d1ca3d7cfeca610cf26167c62ef64b291650b3f
Tomislav Dukez <tomdu3@ymail.com>
2023-12-13 20:21:50 +0000

    Update: variables types

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,5 +1,11 @@
 fn main() {
     let mut something = 4;
-    something = 5;
-    println!("something is: {}", something);
+    const SOMETHING: i32 = 5; // const is always immutable, type must be annotated
+    let something_else = 6; // something_else is immutable
+    // something_else = 7; // error: cannot assign twice to immutable variable
+    println!("something is mutable: {}", something);
+    println!("SOMETHING is a constant: {}", SOMETHING);
+    println!("something_else is: {}", something_else);
+
 }
+  
